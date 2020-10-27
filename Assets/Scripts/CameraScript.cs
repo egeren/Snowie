ed8bfc2f65e2c70706e9b4df3e170c6c08f3dead
egeren <32365178+egeren@users.noreply.github.com>
2020-10-28 01:34:43 +0300

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    bool animation = true;
+    Vector3 offset = new Vector3(0, 0.9f, 0);
+    [SerializeField] GameObject player;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(!animation)
+            this.transform.position = new Vector3(0, player.transform.position.y + offset.y, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float forwardSpeed = 3f;
+    [SerializeField] float speed = 1f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rb.position += Vector2.up * forwardSpeed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.position += Vector2.left * speed * Time.deltaTime;
+            rb.position = new Vector3(Mathf.Clamp(rb.position.x, -0.632f, + 0.632f), rb.position.y, 0);
+            rb.MoveRotation(-45);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.position += Vector2.right * speed * Time.deltaTime;
+            rb.position = new Vector3(Mathf.Clamp(rb.position.x, -0.632f, +0.632f), rb.position.y, 0);
+            rb.MoveRotation(45);
+        }
+        else
+            rb.MoveRotation(0);
+    }
+}
